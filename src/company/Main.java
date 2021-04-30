5aa9b8e540d2bd862fdf8b5c3ab4abdcc4071005
dreedski <dreedski@hotmail.com>
2021-04-29 21:59:46 -0500

    added play again loop

--- src/company/Main.java
+++ src/company/Main.java
@@ -8,22 +8,26 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
         String playAgain = "";
+        int numberOfTries = 0;
         do {
             int theNumber = (int) (Math.random() * 100 + 1);
             int guess = 0;
             while (guess != theNumber) {
                 System.out.print("Guess a number between 1 and 100: ");
                 guess = in.nextInt();
+                numberOfTries = numberOfTries + 1;
                 if (guess < theNumber) {
                     System.out.println(guess + " is too low. Try again.");
                 } else if (guess > theNumber) {
                     System.out.println(guess + " is too high. Try again.");
                 } else {
-                    System.out.println(guess + " is correct. You win!!!");
+                    System.out.println(guess + " is correct. You win!!! It took you " + numberOfTries + " to find the answer!");
                     System.out.println("Would you like to play again y/n?");
                     playAgain = in.next();
                 }
             }
         }while (playAgain.equalsIgnoreCase("y")) ;
+        System.out.println("Thank you for playing! Goodbye!");
+        in.close();
     }
 }
